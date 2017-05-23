06508352045755ed89cf6fb981c38e1fbeee021c
andreiulinici <andrei.ulinici1@gmail.com>
2017-05-23 18:34:15 +0300

    Added login, logout, register functionalities

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 // Using express: http://expressjs.com/
 var express = require('express');
 
-// Create the app
+// Init App
 var app = express();
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 
 // File System for loading the list of words
 var fs = require('fs');
@@ -12,34 +24,70 @@ var fs = require('fs');
 var cors = require('cors');
 app.use(cors());
 
-// This is for hosting files
-app.use(express.static('Public'));
+mongoose.connect('mongodb://localhost/fleetmanagement');
+var db = mongoose.connection;
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'l33t-SQL2017',
-    database: 'fleet_management'
-});
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
     }
-    
-    console.log('connected as id ' + connection.threadId);
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
 
-connection.query('SELECT * FROM fleet_management.driver', function (error, results, fields) {
-    if (error) throw error;
-    console.log(results);
-});
 
-connection.end();
-//app.get('/', function(req, resp) {});
+
+app.use('/', routes);
+app.use('/users', users);
 
 
 // Set up the server
